917166bace68cd04d9560929cfbf793f9c85949c
Nicolas Guerineau <nicolasguerineau@MacBook-Air.com>
2022-03-11 19:42:20 +0100

    redirect file added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,44 +7,37 @@ import "../styles/filter.css";
 import OfferByPage from "../components/OfferByPage";
 
 const Home = ({ searchField }) => {
-	const [values, setValues] = useState([0, 500]);
-	const [finalValues, setFinalValues] = useState([0, 500]);
-	const [checked, setChecked] = useState(false);
-	const [offerByPage, setOfferByPage] = useState(12);
-	const [currentPage, setCurrentPage] = useState(1);
+  const [values, setValues] = useState([0, 500]);
+  const [finalValues, setFinalValues] = useState([0, 500]);
+  const [checked, setChecked] = useState(false);
+  const [offerByPage, setOfferByPage] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
 
-	return (
-		<main>
-			<hr />
-			<div className="filters-container">
-				<Switch
-					checked={checked}
-					setChecked={setChecked}
-					setCurrentPage={setCurrentPage}
-				/>
-				<RangeFilter
-					values={values}
-					setValues={setValues}
-					finalValues={finalValues}
-					setFinalValues={setFinalValues}
-					setCurrentPage={setCurrentPage}
-				/>
-				<OfferByPage
-					setOfferByPage={setOfferByPage}
-					setCurrentPage={setCurrentPage}
-				/>
-			</div>
-			<Banner />
-			<Carousel
-				searchField={searchField}
-				finalValues={finalValues}
-				checked={checked}
-				offerByPage={offerByPage}
-				currentPage={currentPage}
-				setCurrentPage={setCurrentPage}
-			/>
-		</main>
-	);
+  return (
+    <main>
+      <hr />
+      <div className="filters-container">
+        <Switch checked={checked} setChecked={setChecked} setCurrentPage={setCurrentPage} />
+        <RangeFilter
+          values={values}
+          setValues={setValues}
+          finalValues={finalValues}
+          setFinalValues={setFinalValues}
+          setCurrentPage={setCurrentPage}
+        />
+        <OfferByPage setOfferByPage={setOfferByPage} setCurrentPage={setCurrentPage} />
+      </div>
+      <Banner />
+      <Carousel
+        searchField={searchField}
+        finalValues={finalValues}
+        checked={checked}
+        offerByPage={offerByPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </main>
+  );
 };
 
 export default Home;
